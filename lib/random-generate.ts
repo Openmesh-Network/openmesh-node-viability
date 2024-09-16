7a6b3163e35e457e7b261f50fb86f3e9348cb8df
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2024-09-16 12:14:57 +0200

    fix: comment out unused code

--- lib/random-generate.ts
+++ lib/random-generate.ts
@@ -1,42 +1,40 @@
-import { type ChainData } from "@/config/chains"
-
 export type ProviderData = number
 export type DailyData = {
   date: string
   price: number
 }
 
-export function generateYearlyData(
-  basePrice: ChainData["basePrice"],
-): DailyData[] {
-  const yearlyData: DailyData[] = []
-  const startDate = new Date()
-  startDate.setFullYear(2023)
+// export function generateYearlyData(
+//   basePrice: ChainData["basePrice"],
+// ): DailyData[] {
+//   const yearlyData: DailyData[] = []
+//   const startDate = new Date()
+//   startDate.setFullYear(2023)
 
-  const randomShift = Math.random() * 4
-  for (let i = 0; i < 365; i++) {
-    const currentDate = new Date(startDate)
-    currentDate.setDate(startDate.getDate() + i)
-    const month = currentDate.getMonth()
+//   const randomShift = Math.random() * 4
+//   for (let i = 0; i < 365; i++) {
+//     const currentDate = new Date(startDate)
+//     currentDate.setDate(startDate.getDate() + i)
+//     const month = currentDate.getMonth()
 
-    const seasonalFactor = 1 + Math.sin(month / 2 - randomShift) * 0.2
-    const randomFactor = 1 + (Math.random() - 0.5) * 0.05
-    const monthlyFactor = seasonalFactor * randomFactor
-    const dailyFactor = 1 + (Math.random() - 0.5) * 0.05
-    const price =
-      Math.round(basePrice * monthlyFactor * dailyFactor * 100) / 100
+//     const seasonalFactor = 1 + Math.sin(month / 2 - randomShift) * 0.2
+//     const randomFactor = 1 + (Math.random() - 0.5) * 0.05
+//     const monthlyFactor = seasonalFactor * randomFactor
+//     const dailyFactor = 1 + (Math.random() - 0.5) * 0.05
+//     const price =
+//       Math.round(basePrice * monthlyFactor * dailyFactor * 100) / 100
 
-    const dailyData: DailyData = {
-      date: currentDate.toLocaleDateString("en-US", {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-      }),
-      price,
-    }
+//     const dailyData: DailyData = {
+//       date: currentDate.toLocaleDateString("en-US", {
+//         day: "2-digit",
+//         month: "long",
+//         year: "numeric",
+//       }),
+//       price,
+//     }
 
-    yearlyData.push(dailyData)
-  }
+//     yearlyData.push(dailyData)
+//   }
 
-  return yearlyData
-}
+//   return yearlyData
+// }
